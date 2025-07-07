b17edb48ceb3f401db9a36d1fd8168f1ecbedcb4
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-07-07 21:56:48 +0200

    Nyx propagation
       * provide spatial_propagation_mut and immutable
       * add SP3/A example
       * dump the OD-residuals
       * to be continued
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/nyx.rs
+++ src/nyx.rs
@@ -5,11 +5,14 @@
  *
  * Documentation: https://github.com/rtk-rs/sp3
  *
- * The nyx feature is released under AGPLv3
+ * The Nyx feature is released under AGPLv3
  * Copyright (C) 2021-onward Christopher Rabotin <christopher.rabotin@gmail.com> et al. (cf. AUTHORS.md)
  * Documentation: https://nyxspace.com/
  */
-use crate::prelude::{Duration, Epoch, SP3, SV};
+use log::error;
+use thiserror::Error;
+
+use crate::prelude::{Duration, Epoch, SP3Entry, SP3Key, SP3, SV};
 
 use anise::{
     constants::{
@@ -19,90 +22,230 @@ use anise::{
     prelude::Almanac,
 };
 
-use std::{collections::BTreeMap, sync::Arc};
+use std::sync::Arc;
+
+use hifitime::{TimeSeries, Unit};
 
 use nyx_space::{
-    dynamics::{OrbitalDynamics, SolarPressure, SpacecraftDynamics},
-    od::{kalman::KalmanVariant, SpacecraftKalmanOD},
-    propagators::{IntegratorOptions, Propagator},
+    dynamics::{
+        DynamicsError as NyxDynamicsError, OrbitalDynamics, SolarPressure, SpacecraftDynamics,
+    },
+    md::trajectory::Traj,
+    propagators::Propagator,
+    Spacecraft,
 };
 
+/// [SP3] prediction specific errors.
+#[derive(Debug, Error)]
+pub enum PredictionError {
+    #[error("undetermined initial state")]
+    UndeterminedInitialState,
+
+    #[error("dynamics error: {0}")]
+    NyxDynamics(#[from] NyxDynamicsError),
+}
+
 impl SP3 {
-    /// Propagate this [SP3] into the future, returning a new extended [SP3].
-    /// Refer to [Self::propagate_mut] for more information.
-    pub fn propagate(&self, almanac: Arc<Almanac>, prediction_duration: Duration) -> Self {
-        let mut s = self.clone();
-        s.propagate_mut(almanac, prediction_duration);
-        s
+    /// Obtain a [Spacecraft] model, at desired [Epoch] for desired [SV]
+    pub fn spacecraft_model(&self, epoch: Epoch, sv: SV) -> Option<Spacecraft> {
+        self.spacecraft_model_iter(epoch)
+            .filter_map(|(sv_i, sc_i)| if sv_i == sv { Some(sc_i) } else { None })
+            .reduce(|k, _| k)
     }
 
-    /// Propagate this (entire) [SP3] in the future, using [Propagator::dp78]
-    /// and mutable access.
+    /// Obtain a [Spacecraft] model at desired [Epoch], for each satellite
+    pub fn spacecraft_model_iter(
+        &self,
+        epoch: Epoch,
+    ) -> Box<dyn Iterator<Item = (SV, Spacecraft)> + '_> {
+        Box::new(self.satellites_orbit_iter(EARTH_J2000).filter_map(
+            move |(orbit_t, orbit_sv, orbit)| {
+                if orbit_t == epoch {
+                    let sc_model = Spacecraft::builder().orbit(orbit).build();
+                    Some((orbit_sv, sc_model))
+                } else {
+                    None
+                }
+            },
+        ))
+    }
+
+    /// Obtain a predicted [Traj]ectory for each satellite.
     ///
     /// ## Input
-    /// - almanac: [Almanac]
-    /// - prediction_duration: [Duration] of the prediction
-    pub fn propagate_mut(&mut self, almanac: Arc<Almanac>, prediction_duration: Duration) {
-        let opts = IntegratorOptions::with_fixed_step(self.header.sampling_period);
-
+    /// - almanac: [Almanac] definition
+    /// - initial_epoch: possible initial [Epoch].
+    /// When undefined, we simply use the latest state in time.
+    /// - prediction_duration: [Duration] of the predicted [Trajectory]
+    ///
+    /// ## Returns
+    /// - ([SV], [Trajectory]) for each satellite
+    /// - [PredictionError]
+    pub fn trajectory_predictions_iter(
+        &self,
+        almanac: Arc<Almanac>,
+        initial_epoch: Option<Epoch>,
+        prediction_duration: Duration,
+    ) -> Result<Box<dyn Iterator<Item = (SV, Traj<Spacecraft>)> + '_>, PredictionError> {
         let orbital_model = OrbitalDynamics::point_masses(vec![MOON, SUN]);
 
-        let srp_model = SolarPressure::new(vec![EARTH_J2000, MOON_J2000], almanac.clone())
-            .unwrap_or_else(|e| {
-                // TODO replace with proper error
-                panic!("failed to build solar pressure model: {}", e);
-            });
+        let srp_model = SolarPressure::new(vec![EARTH_J2000, MOON_J2000], almanac.clone())?;
 
         let dynamics = SpacecraftDynamics::from_model(orbital_model, srp_model);
 
-        // Propagates each satellite, arming the propagator using last described precise position.
-        // There should be better approach by taking all the data set into account, reducing the propagtion error.
-        let last_epoch = self.last_epoch().expect("empty data set"); // TODO replace with proper error
+        let initial_epoch = match initial_epoch {
+            Some(initial_epoch) => initial_epoch,
+            None => self
+                .last_epoch()
+                .ok_or(PredictionError::UndeterminedInitialState)?,
+        };
 
-        let final_states = self
-            .data
-            .iter()
-            .filter(|(k, v)| k.epoch == last_epoch)
+        let last_epoch = initial_epoch + prediction_duration;
+
+        // create a propagator for each satellite
+        // using the same solar pressure and dynamics model
+        let iter = self
+            .spacecraft_model_iter(initial_epoch)
+            .filter_map(move |(sv, sc_model)| {
+                let dynamics = dynamics.clone();
+
+                match Propagator::default(dynamics)
+                    .with(sc_model, almanac.clone())
+                    .until_epoch_with_traj(last_epoch)
+                {
+                    Ok((_final_state, traj)) => Some((sv, traj)),
+                    Err(e) => {
+                        // prediction error (should not happen)
+                        // simply catch it, and the "future" states will be missing
+                        // for those satellites
+                        error!("{}({}) - prediction error: {}", sv, initial_epoch, e);
+                        None
+                    },
+                }
+            });
+
+        Ok(Box::new(iter))
+    }
+
+    /// Predict spatial coordinates for each satellite, for desired duration, returning a
+    /// new [SP3] expanded in time. Refer to [Self::spatial_prediction_mut] for more information.
+    pub fn spatial_prediction(
+        &self,
+        almanac: Arc<Almanac>,
+        initial_epoch: Option<Epoch>,
+        prediction_duration: Duration,
+    ) -> Result<Self, PredictionError> {
+        let mut s = self.clone();
+        s.spatial_prediction_mut(almanac, initial_epoch, prediction_duration)?;
+        Ok(s)
+    }
+
+    /// Predict spatial coordinates for each satellite, for desired duration, with mutable access,
+    /// expanding this [SP3] in the future.
+    ///
+    /// ## Input
+    /// - almanac: [Almanac]
+    /// - initial_epoch: Possible custom [Epoch] offset
+    /// used to determine the initial state. When set to None,
+    /// we will use the latest state described by this [SP3].
+    /// - prediction_duration: [Duration] of the prediction
+    pub fn spatial_prediction_mut(
+        &mut self,
+        almanac: Arc<Almanac>,
+        initial_epoch: Option<Epoch>,
+        prediction_duration: Duration,
+    ) -> Result<(), PredictionError> {
+        // Determine initial state
+        let initial_epoch = match initial_epoch {
+            Some(initial_epoch) => initial_epoch,
+            None => self
+                .last_epoch()
+                .ok_or(PredictionError::UndeterminedInitialState)?,
+        };
+
+        let last_epoch = initial_epoch + prediction_duration;
+
+        let new_epochs = ((last_epoch - initial_epoch).to_unit(Unit::Second)
+            / self.header.sampling_period.to_unit(Unit::Second))
+        .round() as u64;
+
+        // obtain a predicted trajectory for each satellite
+        let trajectories = self
+            .trajectory_predictions_iter(almanac, Some(initial_epoch), prediction_duration)?
             .collect::<Vec<_>>();
 
-        // Create a propagator that uses the same model
-        let setup = Propagator::dp78(dynamics, opts);
+        let timeserie =
+            TimeSeries::inclusive(initial_epoch, last_epoch, self.header.sampling_period);
 
-        for (k, v) in final_states.iter() {
-            // Deploy a process
-            let mut odp = SpacecraftKalmanOD::new(
-                setup.clone(),
-                KalmanVariant::DeviationTracking,
-                None,
-                BTreeMap::new(),
-                almanac.clone(),
-            );
+        // iterate each trajectories and expand self
+        for (sv, traj) in trajectories.iter() {
+            for epoch in timeserie.clone().into_iter() {
+                match traj.at(epoch) {
+                    Ok(state) => {
+                        let pos_vel_km = state.orbit.to_cartesian_pos_vel();
+
+                        let key = SP3Key { sv: *sv, epoch };
+
+                        let value = SP3Entry::from_predicted_position_km((
+                            pos_vel_km[0],
+                            pos_vel_km[1],
+                            pos_vel_km[2],
+                        ))
+                        .with_velocity_km_s((
+                            pos_vel_km[3],
+                            pos_vel_km[4],
+                            pos_vel_km[5],
+                        ));
+
+                        // push new content
+                        self.data.insert(key, value);
+                    },
+                    Err(e) => {
+                        error!("{}({}) - prediction failed with {}", epoch, sv, e);
+                    },
+                }
+            }
         }
+
+        // update self
+        self.header.num_epochs += new_epochs; //TODO: incorrect in case of failure(s)
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::prelude::{Duration, Epoch, Split, SP3};
+    use crate::prelude::{Duration, Epoch, SP3};
     use anise::prelude::Almanac;
     use std::str::FromStr;
     use std::sync::Arc;
 
     #[test]
-    fn test_nyx_propagator() {
+    fn spatial_propagation() {
         let almanac = Arc::new(Almanac::until_2035().unwrap());
 
         // entire setup
         let parsed =
-            SP3::from_gzip_file("data/SP3/C/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz").unwrap();
+            SP3::from_gzip_file("data/SP3/A/NGA0OPSRAP_20251850000_01D_15M_ORB.SP3.gz").unwrap();
 
-        // grab first 12 hours, propagate last 12 hours,
-        let noon = Epoch::from_str("2020-06-25T12:00:00 GPST").unwrap();
-        let (sp3_morning, _) = parsed.split(noon);
+        let midnight = Epoch::from_str("2020-06-25T00:00:00 GPST").unwrap();
+        let noon_offset = midnight + Duration::from_hours(12.0);
 
-        let predicted = sp3_morning.propagate(almanac, Duration::from_hours(12.0));
+        let predicted = parsed
+            .spatial_prediction(almanac, Some(noon_offset), Duration::from_hours(12.0))
+            .unwrap_or_else(|e| {
+                panic!("SP3 (spatial) prediction failed with: {}", e);
+            });
 
-        // compare
+        // obtain residuals
         let residuals = predicted.substract(&parsed);
+
+        // Dump residuals as fake SP3
+        residuals.to_file("od-residuals.txt").unwrap_or_else(|e| {
+            panic!("failed to dump OD residuals.txt: {}", e);
+        });
+
+        // run testbench
     }
 }
